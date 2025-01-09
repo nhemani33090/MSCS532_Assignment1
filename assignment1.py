7b1b7a992d32426a7fad287c73ba2e1b081487e2
nhemani33090 <nhemani33090@ucumberlands.edu>
2025-01-08 22:16:37 -0500

    feat: Sort elements in descending order

--- assignment1.py
+++ assignment1.py
@@ -1,13 +1,13 @@
-def insertion_sort(arr):
+def insertion_sort_descending(arr):
     for i in range(1, len(arr)):
         key = arr[i]
         j = i - 1
-        while j >= 0 and key < arr[j]:
+        while j >= 0 and key > arr[j]:
             arr[j + 1] = arr[j]
             j -= 1
         arr[j + 1] = key
 
 array = [14, 11, 17, 3, 21]
 print("Original array:", array)
-insertion_sort(array)
+insertion_sort_descending(array)
 print("Sorted array:", array)
