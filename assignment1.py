d8f1973c7a09f07ce2d1948ef20dbe1cb216b01b
nhemani33090 <nhemani33090@ucumberlands.edu>
2025-01-08 22:14:39 -0500

    feat: Implement insertion sort

---
+++ assignment1.py
@@ -0,0 +1,13 @@
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and key < arr[j]:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+
+array = [14, 11, 17, 3, 21]
+print("Original array:", array)
+insertion_sort(array)
+print("Sorted array:", array)
